88de71c35d6dbd5ea9a821651ee20d9810ab7ff8
Shepherd <167853997+Shepherd-404@users.noreply.github.com>
2026-02-14 23:13:22 -0600

    Update storage_terminal.js
    change wireless terminal recipe

--- server_scripts/Phase1Starter/Mods/ActuallyAdditions/storage_terminal.js
+++ server_scripts/Phase1Starter/Mods/ActuallyAdditions/storage_terminal.js
@@ -1,9 +1,17 @@
-// Storage terminal (Tom's Simple Storage): replace glowstone with empowered restonia crystal block
+// Tom's Simple Storage terminals: replace glowstone with Actually Additions crystal blocks
 
 ServerEvents.recipes((event) => {
+  // Storage terminal: glowstone dust -> empowered restonia crystal block
   event.replaceInput(
     { output: "toms_storage:storage_terminal" },
     "minecraft:glowstone_dust",
     "actuallyadditions:empowered_restonia_crystal_block"
+  )
+
+  // Wireless terminal: glowstone dust -> empowered diamatine crystal block
+  event.replaceInput(
+    { output: "toms_storage:wireless_terminal" },
+    "minecraft:glowstone_dust",
+    "actuallyadditions:empowered_diamatine_crystal_block"
   )
 })
